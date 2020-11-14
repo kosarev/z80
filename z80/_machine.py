746e8680921a99241f559fce03a30a7414f1040b
Ivan Kosarev <ivan@kosarev.info>
2020-11-14 14:01:46 +0200

    [#7] Refine constructing state images.

--- z80/_machine.py
+++ z80/_machine.py
@@ -7,91 +7,194 @@
 #
 #   Published under the MIT license.
 
-import struct
 from ._z80 import _I8080Machine, _Z80Machine
 
 
-class _StateBase(object):
-    _STATE_FIELDS = {
-        'c': (0, 'B'), 'b': (1, 'B'), 'bc': (0, '<H'),
-        'e': (2, 'B'), 'd': (3, 'B'), 'de': (2, '<H'),
-        'l': (4, 'B'), 'h': (5, 'B'), 'hl': (4, '<H'),
-        'f': (6, 'B'), 'a': (7, 'B'), 'af': (6, '<H'),
-        'pc': (8, '<H'), 'sp': (10, '<H'),
-    }
+class _ByteState(object):
+    def __init__(self, image):
+        assert len(image) >= 1
+        self.__image = image
 
+    def get(self):
+        return self.__image[0]
+
+    def set(self, value):
+        assert 0  # TODO
+        self.__image[0] = value
+
+    value = property(get, set)
+
+
+class _WordState(object):
+    def __init__(self, image):
+        assert len(image) >= 2
+        self.__image = image
+        self.lo = _ByteState(image[0:1])
+        self.hi = _ByteState(image[1:2])
+
+    def get(self):
+        return self.__image[0] + self.__image[1] * 0x100
+
+    def set(self, value):
+        self.__image[0] = value % 0x100
+        self.__image[1] = value // 0x100
+
+    value = property(get, set)
+
+
+class _ImageParser(object):
+    def __init__(self, image):
+        self.__image = image
+
+    def parse_block(self, size):
+        assert len(self.__image) >= size
+        block = self.__image[0:size]
+        self.__image = self.__image[size:]
+        return block
+
+    def parse_rest(self):
+        return self.parse_block(len(self.__image))
+
+    def parse_byte(self):
+        return _ByteState(self.parse_block(1))
+
+    def parse_word(self):
+        return _WordState(self.parse_block(2))
+
+
+class _StateBase(object):
     def __init__(self, image):
         self._image = image
 
-    def get(self, id):
-        offset, format = self._STATE_FIELDS[id]
-        size = struct.calcsize(format)
-        return struct.unpack(format, self._image[offset:offset + size])[0]
+    def __parse_common_fields(self, parser):
+        self.__bc = parser.parse_word()
+        self.__de = parser.parse_word()
+        self.__hl = parser.parse_word()
+        self.__af = parser.parse_word()
+        self.__pc = parser.parse_word()
+        self.__sp = parser.parse_word()
+        self.__wz = parser.parse_word()
+        self.__last_read_addr = parser.parse_word()
 
-    def set(self, id, n):
-        offset, format = self._STATE_FIELDS[id]
-        size = struct.calcsize(format)
-        self._image[offset:offset + size] = struct.pack(format, n)
+        self.__a = self.__af.hi
+        self.__b = self.__bc.hi
+        self.__c = self.__bc.lo
+        self.__d = self.__de.hi
+        self.__e = self.__de.lo
+        self.__f = self.__af.lo
+        self.__h = self.__hl.hi
+        self.__l = self.__hl.lo
+
+    def __parse_memory(self, parser):
+        block = parser.parse_rest()
+        assert len(block) == 0x10000, len(block)
+        self.memory = block
+
+    def get_b(self):
+        return self.__b.value
+
+    def set_b(self, value):
+        self.__b.value = value
 
     def get_c(self):
-        return self.get('c')
+        return self.__c.value
+
+    def set_c(self, value):
+        self.__c.value = value
+
+    def get_d(self):
+        return self.__d.value
+
+    def set_d(self, value):
+        self.__d.value = value
 
     def get_e(self):
-        return self.get('e')
+        return self.__e.value
+
+    def set_e(self, value):
+        self.__e.value = value
 
     def get_bc(self):
-        return self.get('bc')
+        return self.__bc.value
+
+    def set_bc(self, value):
+        self.__bc.value = value
 
     def get_de(self):
-        return self.get('de')
+        return self.__de.value
+
+    def set_de(self, value):
+        self.__de.value = value
+
+    def get_hl(self):
+        return self.__hl.value
+
+    def set_hl(self, value):
+        self.__hl.value = value
 
     def get_pc(self):
-        return self.get('pc')
+        return self.__pc.value
 
-    def set_pc(self, n):
-        self.set('pc', n)
+    def set_pc(self, value):
+        self.__pc.value = value
+
+    def get_sp(self):
+        return self.__sp.value
+
+    def set_sp(self, value):
+        self.__sp.value = value
+
+    b = property(get_b, set_b)
+    c = property(get_c, set_c)
+    d = property(get_d, set_d)
+    e = property(get_e, set_e)
+
+    bc = property(get_bc, set_bc)
+    de = property(get_de, set_de)
+    hl = property(get_hl, set_hl)
+    pc = property(get_pc, set_pc)
+    sp = property(get_sp, set_sp)
 
     def get_memory_byte(self, addr):
-        memory = self._get_memory_view()
-        return memory[addr]
+        return self.memory[addr]
 
     def set_memory_block(self, addr, block):
-        memory = self._get_memory_view()
-        memory[addr:addr + len(block)] = block
+        self.memory[addr:addr + len(block)] = block
 
 
 class I8080State(_StateBase):
     def __init__(self, image):
         _StateBase.__init__(self, image)
-        self._STATE_FIELDS.update({
-            'iff': (16, 'B'), 'int_disabled': (17, 'B'), 'halted': (18, 'B'),
-        })
 
-    def _get_memory_view(self):
-        return self._image[20:]
+        parser = _ImageParser(image)
+        self._StateBase__parse_common_fields(parser)
+        self.__iff = parser.parse_byte()
+        self.__int_disabled = parser.parse_byte()
+        self.__halted = parser.parse_byte()
+        parser.parse_byte()  # Padding.
+        self._StateBase__parse_memory(parser)
 
 
 class Z80State(_StateBase):
     def __init__(self, image):
         super().__init__(image)
-        self._STATE_FIELDS.update({
-            'ixl': (16, 'B'), 'ixh': (17, 'B'), 'ix': (16, '<H'),
-            'iyl': (18, 'B'), 'iyh': (19, 'B'), 'iy': (18, '<H'),
 
-            'alt_c': (20, 'B'), 'alt_b': (21, 'B'), 'alt_bc': (20, '<H'),
-            'alt_e': (22, 'B'), 'alt_d': (23, 'B'), 'alt_de': (22, '<H'),
-            'alt_l': (24, 'B'), 'alt_h': (25, 'B'), 'alt_hl': (24, '<H'),
-            'alt_f': (26, 'B'), 'alt_a': (27, 'B'), 'alt_af': (26, '<H'),
-
-            'r': (28, 'B'), 'i': (29, 'B'), 'iy': (28, '<H'),
-            'iff1': (30, 'B'), 'iff2': (31, 'B'),
-
-            'int_disabled': (32, 'B'), 'halted': (33, 'B'),
-            'int_mode': (34, 'B'), 'index_rp_kind': (35, 'B'),
-        })
-
-    def _get_memory_view(self):
-        return self._image[36:]
+        parser = _ImageParser(image)
+        self._StateBase__parse_common_fields(parser)
+        self.__ix = parser.parse_word()
+        self.__iy = parser.parse_word()
+        self.__alt_bc = parser.parse_word()
+        self.__alt_de = parser.parse_word()
+        self.__alt_hl = parser.parse_word()
+        self.__alt_af = parser.parse_word()
+        self.__ir = parser.parse_word()
+        self.__iff1 = parser.parse_byte()
+        self.__iff2 = parser.parse_byte()
+        self.__int_disabled = parser.parse_byte()
+        self.__halted = parser.parse_byte()
+        self.__int_mode = parser.parse_byte()
+        self.__index_rp_kind = parser.parse_byte()
+        self._StateBase__parse_memory(parser)
 
 
 class _MachineBase(object):
