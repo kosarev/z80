0434456b1895e9d0a9d3f64a6cfde5f83e02370d
Ivan Kosarev <ivan@kosarev.info>
2020-12-25 13:49:34 +0200

    Test byte tags.

---
+++ tests/disasm/bytes.asm
@@ -0,0 +1,7 @@
+    db 0xf3                             ; @@ 0x0000 0xf3
+    db 0xaf                             ; @@ 0x0001 0xaf : Hey.
+    db 0x11                             ; @@ 0x0002 0x11
+---
+    db 0xf3                             ; @@ 0x0000 0xf3
+    db 0xaf                             ; @@ 0x0001 0xaf : Hey.
+    db 0x11                             ; @@ 0x0002 0x11
---
+++ tests/disasm/bytes2.asm
@@ -0,0 +1,6 @@
+    db 0xf3                             ; @@ 0x0000 0xf3
+    db 0xaf                             ; @@ 0x0001 0xaf 0x11 : Hey.
+---
+    db 0xf3                             ; @@ 0x0000 0xf3
+    db 0xaf                             ; @@ 0x0001 0xaf : Hey.
+    db 0x11                             ; @@ 0x0002 0x11
