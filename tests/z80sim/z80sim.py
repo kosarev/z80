268ef7099456482e6f541c88a650c10ee9677a4c
Ivan Kosarev <ivan@kosarev.info>
2021-09-28 21:45:14 +0100

    [#37] Add boilerplate code for the Z80 simulator.

---
+++ tests/z80sim/z80sim.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Transistor-level Z80 CPU simulator.
+#
+# Originated from the Visual 6502 and Z80 Explorer projects.
+#
+# https://github.com/trebonian/visual6502
+# commit badcf8e40be74398ec55593e172acb287ca73e3b of 31 Mar 2020
+#
+# https://github.com/gdevic/Z80Explorer
+# commit c11574c1d80352b355d297ad3ae33701a7110485 of 19 Jan 2021
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
