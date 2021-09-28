beaee0191ee828a1f2ecd5ecce877fa00132f705
Ivan Kosarev <ivan@kosarev.info>
2021-09-28 21:49:17 +0100

    [#37] Load node names.

--- tests/z80sim/z80sim.py
+++ tests/z80sim/z80sim.py
@@ -12,8 +12,42 @@
 # commit c11574c1d80352b355d297ad3ae33701a7110485 of 19 Jan 2021
 
 
+class Z80Simulator(object):
+    def __load_node_names(self):
+        self.__node_ids = {}
+        with open('nodenames.js') as f:
+            for line in f:
+                line = line.rstrip()
+                if line.startswith('// '):
+                    continue
+                if line in ('var nodenames ={', '}'):
+                    continue
+
+                assert line.endswith(',')
+                line = line[:-1]
+                fields = line.split(':')
+
+                id, i = fields
+                assert isinstance(id, str)
+                if id[0] == '_':
+                    id = '~' + id[1:]
+
+                assert isinstance(i, str)
+                i = int(i)
+
+                assert id not in self.__node_ids
+                self.__node_ids[id] = i
+
+    def __load_defs(self):
+        self.__load_node_names()
+        assert 0
+
+    def __init__(self):
+        self.__load_defs()
+
+
 def main():
-    pass
+    Z80Simulator()
 
 
 if __name__ == "__main__":
