9650f66ab4600ff1268cea6010e00fcd6d94e889
Ivan Kosarev <ivan@kosarev.info>
2021-09-28 21:52:57 +0100

    [#37] Load nodes.

--- tests/z80sim/z80sim.py
+++ tests/z80sim/z80sim.py
@@ -12,6 +12,18 @@
 # commit c11574c1d80352b355d297ad3ae33701a7110485 of 19 Jan 2021
 
 
+import ast
+
+
+class Node(object):
+    def __init__(self, id, pullup):
+        self.id, self.pullup = id, pullup
+        self.pulldown = False  # TODO: Correct?
+        self.state = False
+        self.gates = []
+        self.c1c2s = []
+
+
 class Z80Simulator(object):
     def __load_node_names(self):
         self.__node_ids = {}
@@ -38,8 +50,32 @@ class Z80Simulator(object):
                 assert id not in self.__node_ids
                 self.__node_ids[id] = i
 
+    def __load_nodes(self):
+        self.__nodes = {}
+        with open('segdefs.js') as f:
+            for line in f:
+                line = line.rstrip()
+                if line in ('var segdefs = [', ']'):
+                    continue
+
+                assert line.startswith('['), repr(line)
+                fields, = ast.literal_eval(line)
+
+                (i, pull, _) = fields[:3]
+                assert isinstance(i, int)
+                pullup = {'+': True, '-': False}[pull]
+
+                if i in self.__nodes:
+                    n = self.__nodes[i]
+                else:
+                    n = Node(i, pullup)
+                    self.__nodes[i] = n
+
+                assert n.pullup == pullup
+
     def __load_defs(self):
         self.__load_node_names()
+        self.__load_nodes()
         assert 0
 
     def __init__(self):
