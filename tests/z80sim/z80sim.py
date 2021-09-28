2515edb1a06f5edbf58ce2b5e2a9ba8bbc2f0d9b
Ivan Kosarev <ivan@kosarev.info>
2021-09-28 21:55:39 +0100

    [#37] Load transistors.

--- tests/z80sim/z80sim.py
+++ tests/z80sim/z80sim.py
@@ -24,6 +24,12 @@ class Node(object):
         self.c1c2s = []
 
 
+class Transistor(object):
+    def __init__(self, id, gate, c1, c2):
+        self.id, self.gate, self.c1, self.c2 = id, gate, c1, c2
+        self.on = False
+
+
 class Z80Simulator(object):
     def __load_node_names(self):
         self.__node_ids = {}
@@ -73,9 +79,51 @@ class Z80Simulator(object):
 
                 assert n.pullup == pullup
 
+    def __load_transistors(self):
+        self.__trans = {}
+        with open('transdefs.js') as f:
+            for line in f:
+                line = line.rstrip()
+                if line in ('var transdefs = [', ']'):
+                    continue
+
+                assert line.startswith('['), repr(line)
+                line = line.replace('false', 'False').replace('true', 'True')
+                fields, = ast.literal_eval(line)
+
+                (id, gate, c1, c2, bb, _, weak) = fields
+                assert isinstance(id, str)
+                assert isinstance(gate, int)
+                assert isinstance(c1, int)
+                assert isinstance(c2, int)
+                assert isinstance(weak, bool)
+
+                # TODO: The comment in the original source says to
+                # 'ignore all the 'weak' transistors for now'.
+                if weak:
+                    continue
+
+                # TODO: Why the original source does this?
+                if c1 in (self.__ngnd, self.__npwr):
+                    c1, c2 = c2, c1
+
+                t = Transistor(id, gate, c1, c2)
+
+                assert id not in self.__trans
+                self.__trans[id] = t
+
+                self.__nodes[gate].gates.append(t)
+                self.__nodes[c1].c1c2s.append(t)
+                self.__nodes[c2].c1c2s.append(t)
+
     def __load_defs(self):
         self.__load_node_names()
         self.__load_nodes()
+
+        self.__ngnd = self.__node_ids['vss']
+        self.__npwr = self.__node_ids['vcc']
+
+        self.__load_transistors()
         assert 0
 
     def __init__(self):
